baf4d83a8ac42d96a8aaafdc54b345fb21f64fd1
hbruegge <ho.brueggemann@gmx.de>
2025-01-15 18:03:38 +0100

    added another method

--- src/com/stiegelmeyer/hello/Hello.java
+++ src/com/stiegelmeyer/hello/Hello.java
@@ -4,4 +4,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public static void anotherMethod() {
+        System.out.println("Hello GIT!");
+    }
 }
